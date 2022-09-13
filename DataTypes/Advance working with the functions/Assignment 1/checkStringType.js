656b7b048329cfbba96a57861ea7c2ae7f3c3633
NagaveniGopalNaik <nagaveni.naik@robosoftin.com>
2022-09-13 09:08:21 +0530

    update stringtype assessment code

--- DataTypes/Advance working with the functions/Assignment 1/checkStringType.js
+++ DataTypes/Advance working with the functions/Assignment 1/checkStringType.js
@@ -1,19 +1,34 @@
 // Write a function which checks and prints only the string type properties of an object.
 let student = {
+    department : {
+        name : "cse",
+        id : 123,
+        language : {
+            name : "java",
+            name2 : "python"
+        }
+    },
     name : "Sudha",
     id : 1234,
-    place : "bhatkal"
+    place : "bhatkal",
+    
 };
 
 function checkString(obj){
 
- for(let val of Object.values(obj)){
-
-     if(typeof(val) === 'string') console.log(val);
-    
- }
+        let str = "";
+        for(let val of Object.values(obj)){
+            if(typeof(val) === 'string'){
+                str += val+"\n";
+            }
+            else if(typeof(val) === 'object'){
+               str += checkString(val);
+            }
+        }
+       
+    return str;
 
 }
 
 
-checkString(student);
+console.log(checkString(student));
